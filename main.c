92a20bec09e2e9da16e498341e02907558e0486d
Aydemirlive <95748228+Aydemirlive@users.noreply.github.com>
2021-12-11 09:50:30 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    char harf1=97;
+    char harf2;
+    
+    printf("harf1 in degeri:%c\n",harf1);
+    
+    harf2=harf1-32;
+    printf("harf2 in degeri:%c\n",harf2);
+    
+    
+  system("PAUSE");	
+  return 0;
+}
